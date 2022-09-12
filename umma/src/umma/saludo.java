6840d2cb760e2ea38bfbf860db3702d185aa1c4e
RUBEN <RUBEN@DESKTOP-1AOS2E2>
2022-09-12 09:00:53 -0500

    Version1Umma

---
+++ umma/src/umma/saludo.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package umma;
+
+/**
+ * @author RUBEN
+ *
+ */
+public class saludo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo");
+
+	}
+
+}
